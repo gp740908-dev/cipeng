59ba22d538e5d095c7cc826cb61355cc7ab47b90
gp740908-dev <gp740908gp@gmail.com>
2025-12-22 19:10:48 +0700

    opti second fix 6 topbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -94,7 +94,7 @@ export default function Navbar() {
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1], type: 'tween' }}
                 style={{
-                    position: 'fixed',
+                    position: 'absolute',
                     top: 0,
                     left: 0,
                     right: 0,
